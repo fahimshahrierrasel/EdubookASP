86f14d40b4b4677a0a7eac4cc4ba3249f92ca9e4
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-02-26 19:53:16 +0600

    Route configuration add

---
+++ EdubookASP/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.FriendlyUrls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace EdubookASP.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
+        }
+    }
+}
